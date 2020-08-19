c82764b132e96f73b5f011e3486a867e1bb94072
DragKlon <drwoland19@gmail.com>
2020-08-20 00:56:29 +0300

    ThreeDimensialPoint added.
    Part of tests for TriangulationHelper added.

--- LocationTracker.Tests/Helpers.Tests/TriangulationHelperTests.cs
+++ LocationTracker.Tests/Helpers.Tests/TriangulationHelperTests.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LocationTracker.Contracts;
 using LocationTracker.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace LocationTracker.Tests
 {
     [TestClass]
     public class TriangulationHelperTests : TriangulationHelper
     {
+        Mock<TriangulationHelperTests> subject;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            subject = new Mock<TriangulationHelperTests>();
+        }
+
         [TestMethod]
         public void TwoCirclesIntersection_TwoIntersection_ReturnsTwoDifferentPoints()
         {
@@ -17,8 +27,9 @@ namespace LocationTracker.Tests
             double firstRadius = 3;
             double secondRadius = 4;
 
-            var intersections = TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
+            var intersections = subject.Object.TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
 
+            //Expected values were calculated by online circles intersection calculator
             Assert.IsTrue(intersections.Count() == 2);
             Assert.IsTrue(intersections.Any(i => i.XPosition == 3.88) && intersections.Any(i => i.XPosition == 1));
             Assert.IsTrue(intersections.Any(i => i.YPosition == 1.16000000) && intersections.Any(i => i.YPosition == 5));
@@ -32,8 +43,9 @@ namespace LocationTracker.Tests
             double firstRadius = 3;
             double secondRadius = 2;
 
-            var intersections = TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
+            var intersections = subject.Object.TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
 
+            //Expected values were calculated by online circles intersection calculator
             Assert.IsTrue(intersections.Count() == 2);
             Assert.IsTrue(intersections.All(i => i.XPosition == 4));
             Assert.IsTrue(intersections.All(i => i.YPosition == 2));
@@ -47,9 +59,34 @@ namespace LocationTracker.Tests
             double firstRadius = 3;
             double secondRadius = 2;
 
-            var intersections = TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
+            var intersections = subject.Object.TwoCirclesIntersection(firstCenter, secondCenter, firstRadius, secondRadius);
 
+            //Expected values were calculated by online circles intersection calculator
             Assert.IsTrue(intersections.Count() == 0);
+        }
+
+        [TestMethod]
+        public void GetPosition_TwoDimensialReceivers_ReturnsGetTwoDimensialPositionResult()
+        {
+            var getTwoDimensialPositionResultPoint = new TwoDimensialPoint();
+            var receivers = new List<IPoint> { new TwoDimensialPoint(), new TwoDimensialPoint(), new TwoDimensialPoint() };
+            var propogationTimes = new List<double>();
+            subject.Setup(m => m.GetTwoDimensialPosition(receivers.OfType<TwoDimensialPoint>(), propogationTimes, 0)).Returns(getTwoDimensialPositionResultPoint);
+
+            var result = subject.Object.GetPosition(receivers, propogationTimes);
+
+            Assert.AreEqual(getTwoDimensialPositionResultPoint, result);
+        }
+
+        [TestMethod]
+        public void GetPosition_ThreeDimensialReceivers_ReturnsNull()
+        {
+            var receivers = new List<IPoint> { new ThreeDimensialPoint(), new ThreeDimensialPoint(), new ThreeDimensialPoint() };
+            var propogationTimes = new List<double>();
+
+            var result = subject.Object.GetPosition(receivers, propogationTimes);
+
+            Assert.IsNull(result);
         }
     }
 }
---
+++ LocationTracker/Contracts/ThreeDimensialPoint.cs
@@ -0,0 +1,42 @@
+﻿using LocationTracker.Validators;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace LocationTracker.Contracts
+{
+    /// <summary>
+    /// Two-dimensial Point class
+    /// </summary>
+    public class ThreeDimensialPoint : IPoint
+    {
+        /// <summary>
+        /// Gets or sets TwoDimensialPoint's position in X ort
+        /// </summary>
+        public double XPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets TwoDimensialPoint's position in Y ort
+        /// </summary>
+        public double YPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets TwoDimensialPoint's position in Z ort
+        /// </summary>
+        public double ZPosition { get; set; }
+
+        /// <summary>
+        /// Collection of different dimension positions
+        /// </summary>
+        public IEnumerable<double> Positions { get => new List<double> { XPosition, YPosition, ZPosition }; set => Positions = value; }
+
+        /// <summary>
+        /// Gets or sets ThreeDimensialPoint's location as string
+        /// </summary>
+        public string Location
+        {
+            get => $"{XPosition.ToString("0.00000000", CultureInfo.InvariantCulture)}{PublicFields.PositionSeparator}{YPosition.ToString("0.00000000", CultureInfo.InvariantCulture)}" +
+                $"{PublicFields.PositionSeparator}{ZPosition.ToString("0.00000000", CultureInfo.InvariantCulture)}";
+            set => Location = value;
+        }
+    }
+}
